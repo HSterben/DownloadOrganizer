b6e8f89ea7876c8a50a23c020ef4ecb0b37d37a7
OfficialSterben <113056206+OfficialSterben@users.noreply.github.com>
2023-12-30 20:30:15 -0500

    Fixed bugs

--- DownloadOrganizerSetup.bat
+++ DownloadOrganizerSetup.bat
@@ -1,10 +1,12 @@
 @echo off
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
-echo sLinkFile = "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\yourAppName.lnk" >> %SCRIPT%
+echo sLinkFile = "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\DownloadOrganizer.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
 echo oLink.TargetPath = "javaw" >> %SCRIPT%
-echo oLink.Arguments = "-jar ""%~dp0app.jar""" >> %SCRIPT%
+echo oLink.Arguments = "-jar ""%~dp0source\DownloadOrganizer.jar""" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
 cscript /nologo %SCRIPT%
 del %SCRIPT%
+
+start javaw -jar "%~dp0source\DownloadOrganizer.jar"
